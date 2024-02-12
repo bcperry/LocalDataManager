aaf08b47ef93037ce9bb4857d2c14baf27ffbfee
Blaine Perry <blainecperry@gmail.com>
2024-02-11 21:00:12 -0600

    simplified connection, added working notebooks

--- Data_Management/file_manager.py
+++ Data_Management/file_manager.py
@@ -2,6 +2,8 @@
 
 import os
 import sqlite3
+import hashlib
+import logging
 from sqlite3 import Error
 
 class FileManager: 
@@ -11,25 +13,38 @@ class FileManager:
 
     Attributes
     ----------
+    :connection: the database connection object
 
     Methods
     -------
+    :create_management_table: 
+    :create_management_folders:
+    :hash_file:
+    :check_hash:
+    :manage_file:
 
     """
 
-    def __init__(self, db_file):
+    def __init__(self, base_path, db_file):
         """
         Constructs all the necessary attributes for the file manager object.
 
         Parameters
         ----------
-        :db_file: the location of the sqlite database used to manage the files
+        :base_path: the path to the data management system
+        :db_file: the name of the sqlite database used to manage the files
         """
+
+        self.base_path = base_path
+
         try:
-            connection = sqlite3.connect(db_file)
+            self.create_management_folders()
+            connection = sqlite3.connect(os.path.join(base_path,db_file))
+            self.connection = connection
+            self.create_management_table()
         except Error as e:
-            print(e)
-        self.connection = connection
+            logging.info(e)
+
     
     def create_management_table(self, SQL=None):
         """ create a file management table in the SQLite database
@@ -40,10 +55,9 @@ class FileManager:
         """
         if SQL is None:
             SQL = """ CREATE TABLE IF NOT EXISTS files (
-                                                id integer PRIMARY KEY,
                                                 name text NOT NULL,
-                                                location text  NOT NULL,
-                                                end_date text  NOT NULL
+                                                hash text PRIMARY KEY,
+                                                location text NOT NULL
                                             ); """
 
 
@@ -51,12 +65,12 @@ class FileManager:
             c = self.connection.cursor()
             c.execute(SQL)
         except Error as e:
-            print(e)
+            logging.info(e)
             return False
 
         return True
 
-    def create_management_folders(self, base_path, data_arch='medallion'):
+    def create_management_folders(self, base_path=r'data/managed_data', data_arch='medallion'):
         """ create the folders required to manage the files
         :param path: string filepath in which to create the management structure
         :param data_arch: [medallion, levels] data scheme for which to create
@@ -69,21 +83,139 @@ class FileManager:
             levels = ['level_1', 'level_2', 'level_3']
 
         if os.path.exists(base_path):
-            print(f'{base_path} exists')
+            logging.info(f'{base_path} exists')
         
         for level in levels:
             if os.path.exists(os.path.join(base_path,level)):
-                print(f'{os.path.join(base_path,level)} exists')
+                logging.info(f'{os.path.join(base_path,level)} exists')
             else:
                 try:
                     os.makedirs(os.path.join(base_path,level))
                 except:
-                    print(f'Failed to create {os.path.join(base_path,level)}')
+                    logging.error(f'Failed to create {os.path.join(base_path,level)}')
                     return False
-        
         return True
 
-    
+    def insert_file_into_files(self, name, hash, location):
+        """
+        Create a new file into the files table
+        :param file:
+        :return: file id
+        """
+        sql = ''' INSERT INTO files(name,hash,location)
+                VALUES(?,?,?) '''
+        cur = self.connection.cursor()
+        cur.execute(sql, (name, hash, location))
+        self.connection.commit()
+        return cur.lastrowid
+
+    def hash_file(self, file):
+        """
+        Provides hashing functionality.
+
+        Parameters:
+            file:
+
+        Returns:
+            The sha256 hash in hex format 
+
+        """
+
+        # A arbitrary (but fixed) buffer size
+        # 65536 = 65536 bytes = 64 kilobytes
+        BUF_SIZE = 65536
+
+        # Initializing the sha256() method
+        sha256 = hashlib.sha256()
+
+        # Opening the file provided
+        while True:
+            data = file.read()
+
+            # True if eof = 1
+            if not data:
+                break
+
+            # Passing that data to that sh256 hash 
+            # function (updating the function with that data)
+            sha256.update(data)
+
+        # sha256.hexdigest() hashes all the input data passed
+        # to the sha256() via sha256.update()
+        # Acts as a finalize method, after which 
+        # all the input data gets hashed
+        # hexdigest() hashes the data, and returns 
+        # the output in hexadecimal format
+        return sha256.hexdigest()
+
+    def check_hashes(self, hash):
+        """
+        Provides functionality to check if a file has been hashed previously.
+
+        Parameters:
+            hash:
+
+        Returns:
+            boolean 
+
+        """
+        # Creating cursor object using connection object
+        cursor = self.connection.cursor()
+            
+        # executing our sql query
+        cursor.execute("""SELECT hash FROM files;""")
+            
+        # create a list of all hashes
+        hashlist = [i[0] for i in cursor.fetchall()]
+
+        if hash in hashlist:
+            return True
+        else:
+            return False
+
+    def check_names(self, name):
+        pass
+
+    def save_uploadedfile(self, uploadedfile, data_management, level):
+        """
+        Provides functionality to save documents per the medallion data management
+        system.
+
+        Parameters:
+            file:
+            data_management:
+            level:
+
+        Returns:
+            true or error
+        """
+
+        # check the management system for the file
+        hash = self.hash_file(uploadedfile)
+        file_managed = self.check_hashes(hash)
+        if file_managed:
+            return f"{uploadedfile.name} already managed."
+
+        # check if a file with this name already exists, if the hash is different
+        file_name = self.check_names(uploadedfile.name)
+
+        try:
+            path = data_management['base_filepath'] + "/" + medallion
+
+        except:
+            return {"data_management": data_management, 
+                    "message": "Failed to parse management json"}
+
+
+        try:
+            with open(os.path.join(path, file_name),"wb") as f:
+                f.write(uploadedfile.getbuffer())
+
+            return True
+        except Exception as err:
+            logging.error(err)
+            return err
+
 
 if __name__ == '__main__':
     pass
--- test/test_file_management.py
+++ test/test_file_management.py
@@ -1,26 +1,69 @@
 '''Testing for file_manager.py'''
 
 import unittest
+import os
+import shutil
+
+
+
 
 class TestFileManager(unittest.TestCase):
     '''For testing the File class'''
 
+    @classmethod
+    def tearDownClass(self) -> None:
+        self.manager.connection.close()
+        shutil.rmtree('data')
+
+    @classmethod
+    def setUpClass(self):
+
+        # create the required folder
+        if not os.path.exists('data'):
+            try:
+                os.makedirs('data')
+            except:
+                print(f'Failed to create data directory')
+                pass
+        
+        # stand up the file manager
+        from Data_Management.file_manager import FileManager
+        self.manager = FileManager(base_path= 'data', db_file='testFile.db')
+        self.manager.create_management_table()
+        self.manager.insert_file_into_files('name', 'hash', 'location')
+
+
     def test_import(self):
-        """ Test that the Processing module can be imported. """
+        """ Test that the Data Management module can be imported. """
         import Data_Management
 
     def test_create_management_table(self):
         """ Test the database connection. """
-        from Data_Management.file_manager import FileManager
-        f = FileManager(r'data/testFile.db')
-        table = f.create_management_table()
+        table = self.manager.create_management_table()
+        self.manager.create_management_table()
         self.assertTrue(table)
 
     def test_create_management_folders(self):
-        from Data_Management.file_manager import FileManager
-        f = FileManager(r'data/testFile.db')
-        manager = f.create_management_folders(r'data/managed')
+        """ Test the creation of the Folder structure"""
+        manager = self.manager.create_management_folders(r'data/managed')
         self.assertTrue(manager)
+
+    def test_hash_file(self):
+        """ Test the file hashing"""
+
+        # Reading a file 
+        file = open(r'test/testfile.txt', 'r', encoding='utf-8')
+        print(file.read())
+        hash = self.manager.hash_file(file)
+        self.assertEquals(hash , 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855')
+
+    def test_insert_file_into_files(self):
+        result = self.manager.insert_file_into_files('name.txt', 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855', 'location')
+        self.assertGreaterEqual(result, 0)
+
+    def test_check_hashes(self):
+        self.assertTrue(self.manager.check_hashes('hash'))
+        self.assertFalse(self.manager.check_hashes('not_here'))
 
 if __name__ == '__main__':
     unittest.main()
