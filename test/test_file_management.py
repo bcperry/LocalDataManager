72fec31e7376276f14c2a39444380d7de2080e81
bcperry <blainecperry@gmail.com>
2024-04-21 16:49:21 -0500

    coorected test case

--- test/test_file_management.py
+++ test/test_file_management.py
@@ -52,7 +52,7 @@ class TestFileManager(unittest.TestCase):
         file = open(r'test/testfile.txt', mode='rb')
         # print(file.read())
         hash = self.manager.hash_file(file)
-        self.assertEquals(hash , '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08')
+        self.assertEqual(hash , '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08')
 
     def test_insert_file_into_files(self):
         result = self.manager.insert_file_into_files('name.txt', 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855', 'location')
